330287a227240d1c85698d9707627662c5ee60b0
Masahiro Honma <hiratara@cpan.org>
2013-04-09 23:03:37 +0900

    Split into an app class.

--- lib/App/TextSieve.pm
+++ lib/App/TextSieve.pm
@@ -5,6 +5,65 @@ use warnings;
 
 our $VERSION = '0.01';
 
+sub new {
+    my ($class, %params) = @_;
+    my $self = bless {
+        in => \*STDIN,
+        out => \*STDOUT,
+        patterns => [],
+        %params,
+    }, $class;
+
+    {
+        # Disable buffering
+        my $orig_fh = select $self->{out};
+        local $| = 1;
+        select $orig_fh;
+    }
+
+    $self;
+}
+
+sub print_log {
+    my ($self, $line, $fibre) = @_;
+    my $duration = time - $fibre->{start_time};
+    $line =~ s/(\r?\n)$//;
+    print {$self->{out}} $line, "($fibre->{count} times par $duration sec)", $1;
+}
+
+sub run {
+    my $self = shift;
+    my %fibres;
+    my $skipped = 0;
+    my $in = $self->{in};
+    LOOP: while (my $line = <$in>) {
+        print "\r" if $skipped;
+        for my $pat (@{$self->{patterns}}) {
+            if ($line =~ $pat->{regex}) {
+                my $name = $pat->{name};
+                my $fibre = $fibres{$name} //= {
+                    count => 0, start_time => time,
+                };
+                $fibre->{count}++;
+                my $duration = time - $fibre->{start_time};
+                if (
+                    $pat->{max} && $fibre->{count} >= $pat->{max} ||
+                    $pat->{duration} && $duration >= $pat->{duration}
+                ) {
+                    $skipped = 0;
+                    $self->print_log($line, delete $fibres{$name});
+                } else {
+                    $skipped++;
+                    print {$self->{out}} "skipped $skipped lines";
+                }
+
+                next LOOP;
+            }
+        }
+        $skipped = 0;
+        print {$self->{out}} $line;
+    }
+}
 
 1;
 __END__
